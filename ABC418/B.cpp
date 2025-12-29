3eb706877247ddf8d995b47b5a8254ad272e6af4
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2025-12-29 17:27:27 +0900

    B問題WA

--- ABC418/B.cpp
+++ ABC418/B.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (double i = 0; i < (n); ++i)
 #define ll long long
 using namespace std;
 
@@ -8,24 +8,25 @@ int main() {
     // tの位置を把握して、その中から2つ選び、それらのtに挟まれる文字列の充填率を計算、最大値を更新していく。
     // 最悪計算量は、100C2 * 100なので、多分間に合う。
     // 区間内のtの個数をどうやって得ようか。for文でカウントしてみるか。
-    vector<int> l;
+    vector<double> l;
     double ans = 0;
-    rep(i, int(s.size())) {
+    rep(i, double(s.size())) {
         if (s[i] == 't') {
             l.push_back(i);
         }
     }
-    // rep(i, int(l.size())) cout << l.at(i) << endl;
-    rep(i, int(l.size())) rep(j, i) {
+    // rep(i, double(l.size())) cout << l.at(i) << endl;
+    rep(i, double(l.size())) rep(j, i) {
         // cout << l.at(i) << " " << l.at(j) << endl;
         if (l.at(i) - l.at(j) >= 3) {
-            int count = 0;
-            for (int k = l.at(j); k < l.at(i); k++) {
+            double count = 0;
+            for (double k = l.at(j); k < l.at(i) + 1; k++) {
                 if (s[k] == 't') {
                     count++;
                 }
             }
             // cout << count << endl;
+            // cout << (count - 2) / (l.at(i) - l.at(j) + 1 - 2) << endl;
             double f = (count - 2) / (l.at(i) - l.at(j) + 1 - 2);
             // cout << f << endl;
             if (f > ans) {
@@ -34,6 +35,6 @@ int main() {
         }
     }
         
-    cout << ans << endl;
+    cout << fixed << setprecision(10) << ans << endl;
     return 0;
 }
