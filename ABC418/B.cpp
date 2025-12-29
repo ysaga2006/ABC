cb638df2ebb931101fac5a65d4f0fcf76408e653
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2025-12-29 10:58:28 +0900

    B問題の途中

--- ABC418/B.cpp
+++ ABC418/B.cpp
@@ -8,27 +8,32 @@ int main() {
     // tの位置を把握して、その中から2つ選び、それらのtに挟まれる文字列の充填率を計算、最大値を更新していく。
     // 最悪計算量は、100C2 * 100なので、多分間に合う。
     // 区間内のtの個数をどうやって得ようか。for文でカウントしてみるか。
-    int n = s.size();
-    vector<int> l(0);
+    vector<int> l;
     double ans = 0;
-    rep(i, n) {
+    rep(i, int(s.size())) {
         if (s[i] == 't') {
             l.push_back(i);
         }
     }
-    int m = l.size();
-    rep(i, m) rep(j, i) {
-        int count = 0;
-        for (int k = l.at(i); k < l.at(j); j++) {
-            if (s[k] == 't') {
-                count++;
+    // rep(i, int(l.size())) cout << l.at(i) << endl;
+    rep(i, int(l.size())) rep(j, i) {
+        // cout << l.at(i) << " " << l.at(j) << endl;
+        if (l.at(i) - l.at(j) >= 3) {
+            int count = 0;
+            for (int k = l.at(j); k < l.at(i); k++) {
+                if (s[k] == 't') {
+                    count++;
+                }
+            }
+            // cout << count << endl;
+            double f = (count - 2) / (l.at(i) - l.at(j) + 1 - 2);
+            // cout << f << endl;
+            if (f > ans) {
+                ans = f;
             }
         }
-        double f = (count - 2) / (l.at(j) - l.at(i) + 1 - 2);
-        if (f > ans) {
-            ans = f;
-        }
     }
+        
     cout << ans << endl;
     return 0;
 }
