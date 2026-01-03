890ed77cac0c42fdab3bd4a3d066c4748f6c59f4
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-01-03 21:29:31 +0900

    C問題WA一回目

--- ABC439/C.cpp
+++ ABC439/C.cpp
@@ -4,6 +4,25 @@
 using namespace std;
 
 int main() {
-    
+    int n; cin >> n;
+    set<int> st;
+    int max = 0;
+    rep(i, n) {
+        if (i * i > n) {
+            max = i;
+            break;
+        } 
+    }
+    max--;
+    rep(i, max) rep(j, i) {
+        if (((i + 1) * (i + 1)) + ((j + 1) * (j + 1)) <= n) {
+            st.insert(((i + 1) * (i + 1)) + ((j + 1) * (j + 1)));
+        }
+    }
+    cout << st.size() << endl;
+    for (auto num : st) {
+        cout << num << " ";
+    }
+    cout << endl;
     return 0;
 }
