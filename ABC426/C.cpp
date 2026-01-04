891748fb0dc08b3ad4acaafcb409995cb4ae99bb
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-01-04 20:44:36 +0900

    C問題TLE

--- ABC426/C.cpp
+++ ABC426/C.cpp
@@ -1,19 +1,20 @@
 #include <bits/stdc++.h>
 #define rep(i, n) for (int i = 0; i < (n); ++i)
+#define ll long long
 using namespace std;
 
 int main() {
-    int n, q;
-    cin >> n >> q;
-    vector<int> os(n);
-    rep(i, n) os.at(i) = i + 1;
-    rep(i, q) {
-        int x, y;
-        cin >> x >> y;
-        sort(os.begin(), os.end());
-        fill(os.begin(), lower_bound(os.begin(), os.end(), x + 1) - 1, y);
-        auto it  = lower_bound(os.begin(), os.end(), x + 1) - 1;
-        cout << distance(os.begin(), it) << endl;;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, q; cin >> n >> q;
+    vector<int> v(n + 1); rep(i, n + 1) v.at(i) = i;
+    rep(qi, q) {
+        int x, y; cin >> x >> y;
+        cout << v.at(x) << "\n";
+        for (int i = x + 1; i < y; i++) {
+            v.at(i) -= v.at(x);
+        }
+        rep(i, x) v.at(i) = 0;
     }
     return 0;
 }
