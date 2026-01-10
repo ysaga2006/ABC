26838dc4659ec3b7df5826723f18b48275410c96
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-01-10 13:51:46 +0900

    B問題途中まで

--- ABC410/B.cpp
+++ ABC410/B.cpp
@@ -5,6 +5,16 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+    int n, q; cin >> n >> q;
+    vector<int> v(n + 1);
+    for (;q--;) {
+        int x; cin >> x;
+        if (x > 0) {
+            v.at(x)++;
+            cout << x << endl;
+        } else {
+            
+        }
+    }
     return 0;
 }
