828dbd90bc709509429241db169d4e74d1ed1da8
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-01-10 20:10:58 +0900

    B問題途中経過

--- ABC410/B.cpp
+++ ABC410/B.cpp
@@ -6,15 +6,28 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int n, q; cin >> n >> q;
+    int min = 100;
     vector<int> v(n + 1);
-    for (;q--;) {
+    for (int qi = 0; qi < q; qi++) {
         int x; cin >> x;
         if (x > 0) {
             v.at(x)++;
-            cout << x << endl;
+            cout << x << " ";
         } else {
-            
+            for (int j = 0; j < n; j++) {
+                if (v.at(j) <= min) {
+                    min = v.at(j);
+                }
+            }
+            for (int k = 0; k < n; k++) {
+                if (v.at(k) == min) {
+                    v.at(k)++;
+                    cout << k + 1 << " ";
+                    break;
+                }
+            }
         }
     }
+    cout << endl;
     return 0;
 }
