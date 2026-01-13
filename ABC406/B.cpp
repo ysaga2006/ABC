3bc29068e1cddb4f0f6630463d3f5bf7679e0c32
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-01-13 12:22:32 +0900

    B問題途中まで

---
+++ ABC406/B.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, k; cin >> n >> k;
+    for (int i = 0; i < ; i++) {
+        
+    }
+    while ()
+    return 0;
+}
