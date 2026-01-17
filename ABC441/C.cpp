5d27591776439d9848d2ba30335c399443784d4b
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-01-17 21:48:47 +0900

    C問題WA

--- ABC441/C.cpp
+++ ABC441/C.cpp
@@ -5,7 +5,24 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n, k, x; cin >> n >> k >> x;
-    
+    ll n, k, x; cin >> n >> k >> x;
+    vector<ll> v(n + 1);
+    for (ll i = 0; i < n; i++) {
+        ll a; cin >> a;
+        v.at(i) = a;
+    }
+    sort(v.begin(), v.end());
+    ll sum = 0;
+    for (ll i = 0; i < n; i++) {
+        if (sum < x) {
+            sum += v.at(i);
+        } else {
+            cout << i + 1 + (n - k)  << endl;
+            break;
+        }
+    }
+    if (sum < x) {
+        cout << "-1" << endl;
+    }
     return 0;
 }
