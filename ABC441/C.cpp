45e33d6a8e4102812405966f39eed30d5b61b039
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-01-17 22:47:55 +0900

    C問題AC

--- ABC441/C.cpp
+++ ABC441/C.cpp
@@ -6,23 +6,35 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     ll n, k, x; cin >> n >> k >> x;
-    vector<ll> v(n + 1);
+    vector<ll> v(n);
     for (ll i = 0; i < n; i++) {
         ll a; cin >> a;
         v.at(i) = a;
     }
     sort(v.begin(), v.end());
     ll sum = 0;
-    for (ll i = 0; i < n; i++) {
+    for (ll i = 0; i < n; ++i) {
         if (sum < x) {
             sum += v.at(i);
-        } else {
-            cout << i + 1 + (n - k)  << endl;
-            break;
+            if (sum >= x) {
+            if (i + (n - k) <= n) {
+                cout << i + 1 + (n - k) << endl;
+                break;
+            } else {
+                cout << -1 << endl;
+                break;
+            }
         }
-    }
-    if (sum < x) {
-        cout << "-1" << endl;
+        }
+        if (sum >= x) {
+            if (i + 1 + (n - k) <= n) {
+                cout << i + 1 + (n - k) << endl;
+                break;
+            } else {
+                cout << -1 << endl;
+                break;
+            }
+        }
     }
     return 0;
 }
