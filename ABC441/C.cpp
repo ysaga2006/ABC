72f0fcbab8b2114ebdea2465f1effe6a5bb91475
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-01-17 21:21:29 +0900

    A, B問題AC

---
+++ ABC441/C.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, k, x; cin >> n >> k >> x;
+    
+    return 0;
+}
