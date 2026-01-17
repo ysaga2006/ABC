fbda1808647984ea251808d10a3b48e09c53fb64
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-01-17 20:58:34 +0900

    C問題途中

--- ABC404/C.cpp
+++ ABC404/C.cpp
@@ -6,18 +6,42 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     ll n, m; cin >> n >> m;
-    vector<ll> v(n + 1);
-    for (;m--;) {
+    vector<vector<ll>> adj(n + 1);
+    vector<ll> deg(n + 1, 0);
+    for (int i = 0; i < m; i++) {
         ll a, b; cin >> a >> b;
-        v.at(a)++;
-        v.at(b)++;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+        deg[a]++;
+        deg[b]++;
     }
+
     bool ok = true;
-    for (ll i = 1; i < n + 1; i++) {
-        if (v.at(i) != 2) {
+    for (ll i = 1; i <= n; i++) {
+        if (deg[i] != 2) {
             ok = false;
         }
     }
+
+    if (ok) {
+        vector<bool> visited(n + 1, false);
+        queue<ll> q;
+        q.push(1);
+        visited[1] = true;
+        ll count = 0;
+        while(!q.empty()) {
+            ll u = q.front(); q.pop();
+            count++;
+            for(auto &nx : adj[u]) {
+                if(!visited[nx]) {
+                    visited[nx] = true;
+                    q.push(nx);
+                }
+            }
+        }
+        if (count != n) ok = false;
+    }
+
     cout << (ok ? "Yes" : "No") << endl;
     return 0;
 }
