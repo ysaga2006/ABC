4caf6e27df1ad75b3988d3ae7a45ba08a458f722
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-01-20 22:45:32 +0900

    C問題AC

--- ABC400/C.cpp
+++ ABC400/C.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h>sw
 using ll = long long;
 using namespace std;
 
@@ -8,6 +8,18 @@ int main() {
     ll n; cin >> n;
     ll div2 = sqrt(n / 2) / 1;
     ll div4 = sqrt(n / 4) / 1;
+    if ((div2 + 1) * (div2 + 1) <= n / 2) {
+        div2 += 1;
+    }
+    if ((div4 + 1) * (div4 + 1) <= n / 4) {
+        div4 += 1;
+    }
+    if (div2 * div2 > n / 2) {
+        div2 -= 1;
+    }
+    if (div4 * div4 > n / 4) {
+        div4 -= 1;
+    }
     cout << div2 + div4 << endl; 
     return 0;
 }
