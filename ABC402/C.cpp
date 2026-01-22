c87c2bda7c3494c304140d50b71efaf82b80a705
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-01-22 15:41:20 +0900

    C問題途中経過

---
+++ ABC402/C.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m; cin >> n >> m;
+    vector<vector<int>> v(m, vector<int>());
+    for (;m--;) {
+        int k; cin >> k;
+        for (;k--;) {
+            int a; cin >> a;
+            v.at(m).emplace_back();
+        }
+    }
+    return 0;
+}
