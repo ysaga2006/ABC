4c465c22faa58b105ec2254561ada3066e7c6a37
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-01-23 19:55:45 +0900

    C問題解説つきAC

--- ABC402/C.cpp
+++ ABC402/C.cpp
@@ -6,17 +6,28 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int n, m; cin >> n >> m;
-    vector<vector<int>> v(m, vector<int>());
-    for (;m--;) {
+    vector<vector<int>> v(n, vector<int>(0));
+    vector<int> cnt(0);
+    int ans = 0;
+    for (int i = 0; i < m; i++) {
         int k; cin >> k;
+        cnt.emplace_back(k);
         for (;k--;) {
             int a; cin >> a;
-            v.at(m).emplace_back();
+            a--;
+            v.at(a).emplace_back(i);
         }
     }
     for (;n--;) {
         int b; cin >> b;
-        
+        b--;
+        for (auto ele : v.at(b)) {
+            cnt.at(ele)--;
+            if (cnt.at(ele) == 0) {
+                ans++;
+            }
+        }
+        cout << ans << '\n';
     }
     return 0;
 }
