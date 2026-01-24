fd2a28b2514d7c53cf464cfbcb32a563d4a04b5e
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-01-24 21:08:47 +0900

    A, B問題AC

--- ABC442/A.cpp
+++ ABC442/A.cpp
@@ -5,6 +5,13 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+    string s; cin >> s;
+    int count = 0;
+    for (int i = 0; i < int(s.size()); i++) {
+        if (s[i] == 'i' || s[i] == 'j') {
+            count++;
+        }
+    }
+    cout << count << endl;
     return 0;
 }
