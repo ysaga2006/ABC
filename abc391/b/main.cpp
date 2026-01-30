68ba68d8427ae0569580577411a33431241ecdd6
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-01-30 17:59:18 +0900

    a

--- abc391/b/main.cpp
+++ abc391/b/main.cpp
@@ -16,8 +16,18 @@ int main() {
         string w; cin >> w;
         t.at(i) = w;
     }
+    // 左上のグリッドを(i, j)でfor文を回す。
+    // Tのk(0 < k < m)行目とSの(i + k)行目のj文字目からm文字が等しいかをチェック
+    // cntを持って、mだったらその時点でYesを出力
     for (int i = 0; i < n - m; i++) {
         for (int j = 0; j < n - m; j++) {
+            int cnt = 0;
+            for (int k = 0; k < m; k++) {
+                if (t.at(k) == s.at(i + k).substr(j, m)) cnt++;
+            }
+            if (cnt == m) {
+                cout << i << j << '\n';
+            }
         }
     }
     return 0;
