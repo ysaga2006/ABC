b453bf554ce4e2f2c35597e5513b6198fd7624b6
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-01-31 20:58:39 +0900

    C問題途中

--- abc390/c/main.cpp
+++ abc390/c/main.cpp
@@ -5,6 +5,11 @@ using ll = long long;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+    int h, w; cin >> h >> w;
+    vector<string> v(h);
+    for (int i = 0; i < h; i++) {
+        string s; cin >> s;
+        v.at(i) = s;
+    }
     return 0;
 }
