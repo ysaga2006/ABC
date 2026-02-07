f1bd4cda2dad9b4b2322c595ce7f9367b20b8aaa
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-02-07 19:57:17 +0900

    色々な変更

---
+++ abc086/c/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    pair now = make_pair(0, 0);
+    int n; cin >> n;
+    for (;n--;) {
+        int t, x, y; cin >> t >> x >> y;
+    }
+    return 0;
+}
