32788f98acfe56d0262f07d948212268d9153e25
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-02-14 22:25:14 +0900

    CまでAC

--- abc4xx/abc445/a.cpp
+++ abc4xx/abc445/a.cpp
@@ -5,6 +5,12 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+    string s; cin >> s;
+    int l = s.size();
+    if (s[0] == s[l - 1]) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
     return 0;
 }
--- abc4xx/abc445/b.cpp
+++ abc4xx/abc445/b.cpp
@@ -5,6 +5,24 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+    int n; cin >> n;
+    int upp = 0;
+    vector<string> v(n);
+    for (int i = 0; i < n; i++) {
+        string s; cin >> s;
+        v.at(i) = s;
+        upp = max(int(s.size()), upp);
+    }
+    for (int i = 0; i < n; i++) {
+        int num = (upp - v.at(i).size()) / 2;
+        for (int i = 0; i < num; i++) {
+            cout << ".";
+        }
+        cout << v.at(i);
+        for (int i = 0; i < num; i++) {
+            cout << ".";
+        }
+        cout << '\n';
+    }
     return 0;
 }
