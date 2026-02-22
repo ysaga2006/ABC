8c6b767499a7b36ed84cf1544fc01a1786eef7b7
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-02-22 17:31:38 +0900

    DをAIAC

--- abc4xx/abc446/d/main.cpp
+++ abc4xx/abc446/d/main.cpp
@@ -6,19 +6,13 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int n; cin >> n;
-    vector<int> v(n);
-    set<int> st;
-    for (int i = 0; i < n; i++) {
-        cin >> v.at(i);
+    int ans = 0;
+    unordered_map<int, int> dp;
+    while (n--) {
+        int a; cin >> a;
+        dp[a] = dp[a - 1] + 1;
+        ans = max(ans, dp[a]);
     }
-    int now = v.at(0);
-    for (int i = 0; i < n; i++) {
-        for (auto ele : st) {
-            if (v.at(i + 1) == ele + 1) {
-                st.insert(ele + 1);
-                st.erase(ele);
-            }
-        }
-    }
+    cout << ans << endl;
     return 0;
 }
