b277ccdaef72c31f858922021f683fe4c8d5ee68
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-02-22 12:55:39 +0900

    CまでAC,Dは途中

--- abc4xx/abc446/a/main.cpp
+++ abc4xx/abc446/a/main.cpp
@@ -5,6 +5,8 @@ using ll = long long;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+    string s; cin >> s;
+    s[0] = tolower(s[0]);
+    cout << "Of" << s << endl;
     return 0;
 }
--- abc4xx/abc446/d/main.cpp
+++ abc4xx/abc446/d/main.cpp
@@ -5,6 +5,20 @@ using ll = long long;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+    int n; cin >> n;
+    vector<int> v(n);
+    set<int> st;
+    for (int i = 0; i < n; i++) {
+        cin >> v.at(i);
+    }
+    int now = v.at(0);
+    for (int i = 0; i < n; i++) {
+        for (auto ele : st) {
+            if (v.at(i + 1) == ele + 1) {
+                st.insert(ele + 1);
+                st.erase(ele);
+            }
+        }
+    }
     return 0;
 }
