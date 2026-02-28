5cfa1497a8e88b598a8070cb0fabdc578ad39f48
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-02-28 23:15:43 +0900

    DまでAC

--- abc4xx/abc447/b/main.cpp
+++ abc4xx/abc447/b/main.cpp
@@ -5,6 +5,24 @@ using ll = long long;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+    string s; cin >> s;
+    map<char, int> mp;
+    int upp = 0;
+    for (int i = 0; i < int(s.size()); i++) {
+        mp[s[i]]++;
+        upp = max(mp[s[i]], upp);
+    }
+    set<char> st;
+    for (int i = 0; i < int(s.size()); i++) {
+        if (mp[s[i]] == upp) {
+            st.insert(s[i]);
+        }
+    }
+    for (int i = 0; i < int(s.size()); i++) {
+        if (1 - st.contains(s[i])) {
+            cout << s[i];
+        }
+    }
+    cout << endl;
     return 0;
 }
--- abc4xx/abc447/e/main.cpp
+++ abc4xx/abc447/e/main.cpp
@@ -5,6 +5,9 @@ using ll = long long;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+    int n, m; cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        
+    }
     return 0;
 }
