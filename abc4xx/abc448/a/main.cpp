9eacf9aa96243783b9fab700cb5f3bd4c86b81f8
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-03-08 01:19:34 +0900

    CまでAC

--- abc4xx/abc448/a/main.cpp
+++ abc4xx/abc448/a/main.cpp
@@ -5,6 +5,15 @@ using ll = long long;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+    int n, x; cin >> n >> x;
+    while (n--) {
+        int a; cin >> a;
+        if (a < x) {
+            x = a;
+            cout << 1 << endl;
+        } else {
+            cout << 0 << endl;
+        }
+    }
     return 0;
 }
