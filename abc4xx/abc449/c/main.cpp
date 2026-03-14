55eabdf4ef036740fd4f38317d918ac4f35196c7
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-03-14 21:42:53 +0900

    WA: C

--- abc4xx/abc449/c/main.cpp
+++ abc4xx/abc449/c/main.cpp
@@ -1,10 +1,28 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 using ll = long long;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+    int n, l, r; cin >> n >> l >> r;
+    int count = 0;
+    string s; cin >> s;
+    vector<vector<int>> v(26, vector<int>());
+    for (int i = 0; i < n; i++) {
+        v.at(s[i] - 'a').emplace_back(i);
+    }
+    for (int i = 0; i < 26; i++) {
+        for (int j = 0; j < int(v.at(i).size()); j++) {
+            for (int k = 0; k < j; k++) {
+                int val = v.at(i).at(j) - v.at(i).at(k);
+                if (l <= val && val <= r) {
+                    count++;
+                }
+            }
+        }
+    }
+    cout << count << endl;
     return 0;
 }
