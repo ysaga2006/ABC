77e8e5b5036f44260cc7999c93e2ecd41b31e617
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-03-18 15:30:41 +0900

    WA: B

--- abc4xx/abc406/c/main.cpp
+++ abc4xx/abc406/c/main.cpp
@@ -1,14 +1,10 @@
 #include <bits/stdc++.h>
-using ll = long long;
 using namespace std;
+using ll = long long;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n, k; cin >> n >> k;
-    for (int i = 0; i < ; i++) {
-        
-    }
-    while ()
+    
     return 0;
 }
