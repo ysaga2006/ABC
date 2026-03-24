ea46851b9228acb33061048cbb1f57810d8dc257
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-03-24 19:20:42 +0900

    Set: abc395

---
+++ abc3xx/abc395/c/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n; cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    return 0;
+}
