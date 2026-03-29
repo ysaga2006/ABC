1eb6028320b7a0d36cf7ebc61f74b2700e786667
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-03-29 13:58:01 +0900

    Reset: ABC451 D

--- abc4xx/abc451/d/main.cpp
+++ abc4xx/abc451/d/main.cpp
@@ -1,26 +1,10 @@
 #include <bits/stdc++.h>
-using namespace std;
 using ll = long long;
+using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n; cin >> n;
-    vector beki(10, vector<ll>()); // i桁の冪乗の数をもつ
-    ll num = 1;
-    for (int i = 0; i < 10; i++) {
-        while (num < pow(10, i)) {
-            beki.at(i).push_back(num);
-            num *= 2;
-        }
-    }
     
     return 0;
 }
-
-    // for (int i = 0; i < 10; i++) {
-    //     for (int j = 0; j < int(beki.at(i).size()); j++) {
-    //         cout << beki.at(i).at(j) << " ";
-    //     }
-    //     cout << endl;
-    // }
