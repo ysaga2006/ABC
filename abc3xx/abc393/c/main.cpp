d911608acc88dbbc2b62191e468def8f39e0323f
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-03-29 17:28:52 +0900

    AC: ABC393 C

--- abc3xx/abc393/c/main.cpp
+++ abc3xx/abc393/c/main.cpp
@@ -7,11 +7,15 @@ int main() {
     cin.tie(nullptr);
     int n, m; cin >> n >> m;
     set<pair<int, int>> st;
+    int ans = 0;
     while (m--) {
         int u, v; cin >> u >> v;
-        if (st.contains({u, v})) {
-
+        if (st.contains({u, v}) || st.contains({v, u}) || u == v) {
+            ans++;
+        } else {
+            st.insert({u, v});
         }
     }
+    cout << ans << endl;
     return 0;
 }
