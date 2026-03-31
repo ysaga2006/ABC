4ddd7483cac1f5ad058cacc15137337b15ba9dde
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-03-31 19:03:50 +0900

    AC: ABC394 D

--- abc3xx/abc394/d/main.cpp
+++ abc3xx/abc394/d/main.cpp
@@ -1,10 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+    string s; cin >> s;
+    stack<char> st;
+    for (int i = 0; i < int(s.size()); i++) {
+        if (st.empty()) st.push(s[i]);
+        else if ((st.top() == '(' && s[i] == ')') || (st.top() == '[' && s[i] == ']') || (st.top() == '<' && s[i] == '>')) st.pop();
+        else st.push(s[i]);
+    }
+    cout << (st.empty() ? "Yes" : "No") << '\n';
     return 0;
 }
