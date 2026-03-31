ea78dd23916dcfb210dd0c018cd4117eec459e9f
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-03-31 17:53:12 +0900

    AC: ABC390 C

--- abc3xx/abc390/c/main.cpp
+++ abc3xx/abc390/c/main.cpp
@@ -1,4 +1,5 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 using ll = long long;
 
@@ -6,10 +7,32 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int h, w; cin >> h >> w;
-    vector<string> v(h);
+    int up = 1000, down = -1, left = 1001, right = -1;
+
+    vector<string> v(h); 
+    for (int i = 0; i < h; i++) cin >> v[i];
+
     for (int i = 0; i < h; i++) {
-        string s; cin >> s;
-        v.at(i) = s;
+        for (int j = 0; j < w; j++) {
+            if (v[i][j] == '#') {
+                up = min(up, i);
+                down = max(down, i);
+                left = min(left, j);
+                right = max(right, j);
+            }
+        }
     }
+
+    bool ans = true;
+
+    for (int i = up; i <= down; i++) {
+        for (int j = left; j <= right; j++) {
+            if (v[i][j] == '.') {
+                ans = false;
+            }
+        }
+    }
+
+    cout << (ans ? "Yes" : "No") << endl;
     return 0;
 }
