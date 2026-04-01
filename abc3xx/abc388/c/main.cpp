827c6ffd5f37d2d95d65358ae292b5ec928abbd6
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-04-01 15:47:32 +0900

    WA: abc388 c

--- abc3xx/abc388/c/main.cpp
+++ abc3xx/abc388/c/main.cpp
@@ -5,6 +5,13 @@ using ll = long long;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+    ll n; cin >> n;
+    ll ans = 0;
+    vector<ll> v(n); for (int i = 0; i < n; ++i) cin >> v[i];
+    for (int i = 0; i < n; i++) {
+        ll ok = 0;
+        while (v[ok] * 2 <= v[i]) {ok++; ans++;}
+    }
+    cout << ans << '\n';
     return 0;
 }
