73d105b67c84c73d1cf07b1dba3009285478f1c6
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-04-01 17:49:50 +0900

    wa: abc386 c

--- abc3xx/abc386/c/main.cpp
+++ abc3xx/abc386/c/main.cpp
@@ -5,6 +5,11 @@ using ll = long long;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+    ll ans = -1;
+    ll h, w, d; cin >> h >> w >> d;
+    vector<string> v(h);
+    for (ll i = 0; i < h; i++) {
+        cin >> v[i];
+    }
     return 0;
 }
