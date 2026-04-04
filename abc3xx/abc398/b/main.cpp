690ee38f99b5aa57362f659d39a307b3bde156df
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-04-04 19:26:54 +0900

    update: change indent-width from 4 to 2

--- abc3xx/abc398/b/main.cpp
+++ abc3xx/abc398/b/main.cpp
@@ -3,25 +3,27 @@ using namespace std;
 using ll = long long;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    vector<ll> v(13);
-    for (int i = 0; i < 7; ++i) {
-        ll a; cin >> a; a--;
-        v[a]++;
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  vector<ll> v(13);
+  for (int i = 0; i < 7; ++i) {
+    ll a;
+    cin >> a;
+    a--;
+    v[a]++;
+  }
+  bool duo = false, trio = false;
+  for (int i = 0; i < 13; ++i) {
+    if (v[i] >= 3) {
+      if (trio == false) {
+        trio = true;
+      } else {
+        duo = true;
+      }
+    } else if (v[i] >= 2) {
+      duo = true;
     }
-    bool duo = false, trio = false;
-    for (int i = 0; i < 13; ++i) {
-        if (v[i] >= 3) {
-            if (trio == false) {
-                trio = true;
-            } else {
-                duo = true;
-            }
-        } else if (v[i] >= 2) {
-            duo = true;
-        }
-    }
-    cout << (duo && trio ? "Yes" : "No") << '\n';
-    return 0;
+  }`
+  cout << (duo && trio ? "Yes" : "No") << '\n';
+  return 0;
 }
