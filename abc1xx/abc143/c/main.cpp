5e735b905d038eb0dfce8929483e45dc1047f5d8
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-04-18 14:01:41 +0900

    ac: abc143 c

--- abc1xx/abc143/c/main.cpp
+++ abc1xx/abc143/c/main.cpp
@@ -6,5 +6,20 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   
+
+  ll n; cin >> n;
+  string s; cin >> s;
+
+  vector<ll> ans;
+  ans.push_back(s[0]);
+
+  for (ll i = 1; i < n; ++i) {
+    if (s[i] != ans.back()) {
+      ans.push_back(s[i]);
+    }
+  }
+
+  cout << ans.size() << '\n';
+
   return 0;
 }
