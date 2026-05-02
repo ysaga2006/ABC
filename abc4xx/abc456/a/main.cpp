b6832890a7e25cbb36c661bc6711bfb1e2984ecc
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-05-03 00:51:13 +0900

    ac: abc456 a, b, c, d

--- abc4xx/abc456/a/main.cpp
+++ abc4xx/abc456/a/main.cpp
@@ -6,5 +6,9 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   
+  ll x; cin >> x;
+
+  if (3 <= x && x <= 18) cout << "Yes" << '\n';
+  else cout << "No" << '\n';
   return 0;
 }
