10420344ac58cfbcc8fd145f47f5d2afdc2a5dd5
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-05-09 22:45:50 +0900

    ac: abc457-a, b, c, d && wa: abc457-e

--- abc4xx/abc457/a/main.cpp
+++ abc4xx/abc457/a/main.cpp
@@ -5,6 +5,15 @@ using ll = long long;
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  
+
+  ll n; cin >> n;
+  vector<ll> a(n);
+  for (ll i = 0; i < n; ++i) {
+    cin >> a[i];
+  }
+
+  ll x; cin >> x; x--;
+
+  cout << a[x] << '\n';  
   return 0;
 }
