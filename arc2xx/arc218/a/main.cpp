351e0580bbd19e0c0d48a1ec877a50166b966241
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-05-10 00:29:09 +0900

    fix: initialize input variables and read matrix dimensions

--- arc2xx/arc218/a/main.cpp
+++ arc2xx/arc218/a/main.cpp
@@ -6,5 +6,12 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   
+  ll n, m; cin >> n >> m;
+  
+  vector a(n, vector<ll>(m));
+  for (ll i = 0; i < n; ++i) for (ll j = 0; j < m; ++j) {
+    cin >> a[i][j];
+  }
+
   return 0;
 }
