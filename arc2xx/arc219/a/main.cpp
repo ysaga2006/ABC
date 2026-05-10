39afe900d268b6237c1ca8eae2a476ec1a41de24
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-05-10 21:22:24 +0900

    wa: arc219a

--- arc2xx/arc219/a/main.cpp
+++ arc2xx/arc219/a/main.cpp
@@ -6,5 +6,38 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   
+  ll n, m; cin >> n >> m;
+  
+  vector<string> s(n);
+  for (ll i = 0; i < n; ++i) cin >> s[i];
+
+  vector<bool> visited(n, false);
+
+  string t;
+
+  for (ll i = 0; i < m; ++i) {
+    ll zero = 0, one = 0;
+    for (ll j = 0; j < n; ++j) {
+      if (visited[j] == true) continue;
+      if (s[j][i] == '0') zero++;
+      else one++;
+    }
+      if (zero < one) {
+        t += '1';
+        for (ll j = 0; j < n; ++j) {
+          if (visited[j] == true) continue;
+          if (s[j][i] == '1') visited[j] = true;
+        }
+      }
+      else {
+        t += '0';
+        for (ll j = 0; j < n; ++j) {
+          if (visited[j] == true) continue;
+          if (s[j][i] == '0') visited[j] = true;
+        }
+      }
+  }
+
+  cout << t << '\n';
   return 0;
 }
--- arc2xx/arc219/b/main.cpp
+++ arc2xx/arc219/b/main.cpp
@@ -6,5 +6,15 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   
+  ll t; cin >> t;
+  while (t--) {
+    ll ans = 0;
+    ll n; cin >> n;
+    
+    vector<string> s(n);
+    for (ll i = 0; i < n; ++i) cin >> s[i];
+
+    cout << ans << '\n';
+  }
   return 0;
 }
